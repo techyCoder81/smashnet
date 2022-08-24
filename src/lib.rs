8f5b6abad4f5f7351ba3c36b856d0722fdd3161b
techyCoder81 <mtklein98@gmail.com>
2022-08-24 17:33:14 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,23 @@
 #![feature(c_variadic)]
 use std::{io::{BufWriter, Write}, fs::File, path::Path};
 
-// this package is the curl implementation
-#[cfg(not(feature = "dynamic"))]
-pub use crate::curl::*;
-
-// use types no matter what
-pub use crate::types::*;
-
-// this package is the plugin api (the extern api)
 #[cfg(feature = "dynamic")]
 pub mod api;
-#[cfg(feature = "dynamic")]
-pub use crate::api::*;
-
 #[cfg(not(feature = "dynamic"))]
 mod curl;
-
 pub mod types;
+
+// this package is the curl implementation
+#[cfg(not(feature = "dynamic"))]
+pub use curl::*;
+
+// use types no matter what
+pub use types::*;
+
+// this package is the plugin api (the extern api)
+
+#[cfg(feature = "dynamic")]
+pub use api::*;
 
 #[cfg(not(feature = "dynamic"))]
 #[skyline::main(name = "smashnet")]
