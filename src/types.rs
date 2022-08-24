52ef364efd078f9bdf57718c17a1e8b326ebc90f
techyCoder81 <mtklein98@gmail.com>
2022-08-24 19:54:31 -0400

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -4,7 +4,7 @@ pub struct Curler {
 }
 
 pub trait HttpCurl {
-    fn new() -> Result<Self, ()>;
+    fn new() -> Result<Curler, ()>;
     fn download(&mut self, url: String, location: String) -> Result<(), u32>;
     fn progress_callback(&mut self, callback: fn(f64, f64) -> ()) -> &mut Self;
 }
