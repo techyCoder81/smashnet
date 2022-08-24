d8eb345d57b5f90157a7b89f0290aaa7caf89a4b
techyCoder81 <mtklein98@gmail.com>
2022-08-24 19:00:29 -0400

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,8 @@ use std::{io::{BufWriter, Write}, fs::File, path::Path};
 
 #[cfg(not(feature = "nro"))]
 pub mod api;
-#[cfg(feature = "nro")]
-mod curl;
+
 pub mod types;
-
-// this package is the curl implementation
-#[cfg(feature = "nro")]
-pub use curl::*;
-
-// use types no matter what
-pub use types::*;
 
 // this package is the plugin api (the extern api)
 
