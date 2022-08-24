ea3d6245935ffab6b65f827dde4ddb55e503427d
techyCoder81 <mtklein98@gmail.com>
2022-08-24 15:47:44 -0400

    it works!

--- src/curl.rs
+++ src/curl.rs
@@ -2,6 +2,9 @@ use std::{io::{BufWriter, Write}, fs::File, sync::mpsc::Sender};
 use skyline::libc::*;
 use std::arch::asm;
 use online::sync::check;
+use curl_sys::CURL;
+use std::error::Error;
+use std::path::Path;
 
 //use crate::DownloadInfo;
 
@@ -72,10 +75,146 @@ unsafe extern "C" fn write_fn(data: *const u8, data_size: usize, data_count: usi
     true_size
 }
 
+/// private internal callback handler
+unsafe extern "C" fn callback_internal(callback: extern fn(f64, f64) -> (), dl_total: f64, dl_now: f64, ul_total: f64, ul_now: f64) -> usize {
+    //println!("callback is called: {:p}", callback);
+    if dl_total != 0.0 {
+        callback(dl_total, dl_now);
+    }
+    0
+}
+
+macro_rules! curle {
+    ($e:expr) => {{
+        let result = $e;
+        if result != ::curl_sys::CURLE_OK {
+            Err(result)
+        } else {
+            Ok(())
+        }
+    }}
+}
+
 /*
 static mut START_TICK: usize = 0;
 static mut SENDER: Option<*mut Sender<DownloadInfo>> = None;
+*/
 
+pub struct Curler {
+    callback: Option<fn(f64, f64) -> ()>,
+    curl: *mut CURL,
+}
+
+pub trait HttpCurl {
+    fn new() -> Self;
+    fn is_valid(&mut self) -> Result<&mut Self, u64>;
+    fn download(&mut self, url: String, location: String) -> Result<(), u32>;
+    fn progress_callback(&mut self, callback: fn(f64, f64) -> ()) -> &mut Self;
+}
+
+impl HttpCurl for Curler {
+    fn new() -> Self { 
+        let curl_handle = unsafe { easy_init() };
+        return Curler{callback: None, curl: curl_handle};
+    }
+    fn is_valid(&mut self) -> Result<&mut Self, u64> {
+        if self.curl.is_null() {
+            let error = format!("curl failed to initialize! Pointer value: {:p}", self.curl);
+            println!("{}", error);
+            return Err(self.curl as u64);
+        }
+        return Ok(self);
+    }
+
+    /// download a file from the given url to the given location
+    fn download(&mut self, url: String, location: String) -> Result<(), u32>{
+        // change thread to high priority
+        unsafe {
+            skyline::nn::os::ChangeThreadPriority(skyline::nn::os::GetCurrentThread(), 2);
+        }
+
+        // temp file name: myfile.txt.dl
+        let temp_file = [location.as_str(), ".dl"].concat();
+        if Path::new(temp_file.as_str()).exists() {
+            println!("removing existing temp file: {}", temp_file);
+            std::fs::remove_file(&temp_file);
+        }
+
+        println!("creating temp file: {}", temp_file);
+        let mut writer = std::io::BufWriter::with_capacity(
+            0x40_0000,
+            std::fs::File::create(&temp_file).unwrap()
+        );
+        println!("created bufwriter with capacity");
+        unsafe {
+            let cstr = [url.as_str(), "\0"].concat();
+            let ptr = cstr.as_str().as_ptr();
+            println!("curl is initialized, beginning options");
+            let header = slist_append(std::ptr::null_mut(), "Accept: application/octet-stream\0".as_ptr());
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_URL, ptr))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_HTTPHEADER, header))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_FOLLOWLOCATION, 1u64))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_WRITEDATA, &mut writer))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_WRITEFUNCTION, write_fn as *const ()))?;
+       
+            match self.callback {
+                Some(function) => {
+                    curle!(easy_setopt(self.curl, curl_sys::CURLOPT_NOPROGRESS, 0u64))?;
+                    curle!(easy_setopt(self.curl, curl_sys::CURLOPT_PROGRESSDATA, function as *const ()))?;
+                    curle!(easy_setopt(self.curl, curl_sys::CURLOPT_PROGRESSFUNCTION, callback_internal as *const ()))?;
+                },
+                None => curle!(easy_setopt(self.curl, curl_sys::CURLOPT_NOPROGRESS, 1u64))?,
+            }
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_NOSIGNAL, 1u64))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_SSL_CTX_FUNCTION, curl_ssl_ctx_callback as *const ()))?;
+            curle!(easy_setopt(self.curl, curl_sys::CURLOPT_USERAGENT, "smashnet\0".as_ptr()))?;
+            println!("beginning download.");
+            match curle!(easy_perform(self.curl)){
+                Ok(()) => println!("curl success?"),
+                Err(e) => println!("Error during curl: {}", e) 
+            };
+        }
+
+        println!("flushing writer");
+        writer.flush();
+        println!("dropping writer");
+        std::mem::drop(writer);
+
+
+        // replace/rename the temp file to the expected location
+        if Path::new(location.as_str()).exists() {
+            println!("removing original path: {}", location);
+            std::fs::remove_file(location.as_str());
+        }
+        std::fs::rename(&temp_file, location);
+
+        println!("resetting priority of thread");
+        unsafe {
+            skyline::nn::os::ChangeThreadPriority(skyline::nn::os::GetCurrentThread(), 16);
+        }
+        Ok(())
+    }
+    fn progress_callback(&mut self, function: fn(f64, f64) -> ()) -> &mut Self {
+        self.callback = Some(function);
+        self
+    }
+}
+
+impl Drop for Curler {
+    fn drop(&mut self) {
+        if !self.curl.is_null() {
+            println!("cleaning up curl handle from curler.");
+            unsafe { 
+                match curle!(easy_cleanup(self.curl)) {
+                    Ok(_) => println!("cleaned up curl successfully."),
+                    Err(e) => println!("cleaning up curl failed with error code: {}", e),
+                }; 
+            }
+        }
+    }
+}
+
+/*
 unsafe extern "C" fn progress_func(session: &WebSession, dl_total: f64, dl_now: f64, ul_total: f64, ul_now: f64) -> usize {
     let current_tick = skyline::nn::os::GetSystemTick() as usize;
     let nanoseconds = ((current_tick - START_TICK) * 625) / 12;
@@ -88,17 +227,8 @@ unsafe extern "C" fn progress_func(session: &WebSession, dl_total: f64, dl_now: 
     // session.send_json(&crate::DownloadInfo::new(bps, dl_now, dl_total, "release archive"));
     0
 }
- */
-macro_rules! curle {
-    ($e:expr) => {{
-        let result = $e;
-        if result != ::curl_sys::CURLE_OK {
-            Err(result)
-        } else {
-            Ok(())
-        }
-    }}
-}
+*/
+
 /*
 pub fn try_curl(
     url: &str,
--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,33 @@
 #![feature(proc_macro_hygiene)]
 #![feature(allocator_api)]
-#![feature(asm)]
+//#![feature(asm)]
 #![allow(non_camel_case_types)]
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #![feature(c_variadic)]
 use std::{io::{BufWriter, Write}, fs::File, path::Path};
+use crate::curl::*;
 
 mod curl;
 
 #[skyline::main(name = "smashnet")]
-pub fn main() {
+pub fn main() -> Result<(), u64> {
     curl::install();
     
     println!("starting main for smashnet");
-    unsafe {
-        skyline::nn::os::ChangeThreadPriority(skyline::nn::os::GetCurrentThread(), 2);
-    }
-    let path = "sd:/download_test.txt";
-    // delete the original file if this file already exists on sd
-    if Path::new(path).exists() {
-        println!("removing original path: {}", path);
-        std::fs::remove_file(path);
-    }
-    let mut writer = std::io::BufWriter::with_capacity(
-        0x40_0000,
-        std::fs::File::create(path).unwrap()
-    );
-    println!("created bufwriter with capacity");
-    let url_string = format!("https://github.com/HDR-Development/HDR-Nightlies/releases/download/v0.19.1/hdr_version.txt");
-    let url = url_string.as_str();
-    println!("Curl output: {}", match curl::try_curl_maidenless(url, &mut writer) {
-        Ok(()) => "curl was OK!".to_string(),
-        Err(e) => format!("Failed output: {:?}", e)
-    });
-    println!("flushing writer");
-    writer.flush();
-    println!("dropping writer");
-    std::mem::drop(writer);
-    println!("resetting priority of thread");
-    unsafe {
-        skyline::nn::os::ChangeThreadPriority(skyline::nn::os::GetCurrentThread(), 16);
-    }
+    
+    let path = format!("sd:/content_hashes.txt");
+    let url = format!("https://github.com/HDR-Development/HDR-Releases/releases/download/v0.18.3/content_hashes.txt");
+    match Curler::new()
+        .is_valid()?
+        .progress_callback(
+            |total, progress| println!("Progress: {}", progress/total)
+        )
+        .download(url, path) {
+            Ok(()) => println!("download successful!"),
+            Err(e) => println!("download failed with error code: {}", e)
+        };
+
     println!("Smashnet main has run.");
+    Ok(())
 }
