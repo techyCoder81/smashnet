8588dd0f45d47a37a298b10514132694b92b4dcb
techyCoder81 <mtklein98@gmail.com>
2022-08-24 18:54:43 -0400

    Update api.rs

--- src/api.rs
+++ src/api.rs
@@ -18,21 +18,25 @@ extern "Rust" {
 
 impl HttpCurl for Curler {
     fn new() -> Self {
+        println("running exported new()");
         unsafe {
             HttpCurl__new()
         }
     }
     fn is_valid(&mut self) -> Result<&mut Self, u64> {
+        println("running exported is_valid()");
         unsafe {
             HttpCurl__is_valid(self)
         }
     }
     fn download(&mut self, url: String, location: String) -> Result<(), u32> {
+        println("running exported download()");
         unsafe {
             HttpCurl__download(self, url, location)
         }
     }
     fn progress_callback(&mut self, callback: fn(f64, f64) -> ()) -> &mut Self {
+        println("running exported progress_callback()");
         unsafe {
             HttpCurl__progress_callback(self, callback)
         }
@@ -42,6 +46,7 @@ impl HttpCurl for Curler {
 impl Drop for Curler {
     fn drop(&mut self) {
         unsafe {
+            println("running exported drop()");
             Curler__drop(self)
         }
     }
