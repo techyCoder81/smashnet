e83d5e135cf65e35ac6b44c1d6ddf760a2e51711
techyCoder81 <mtklein98@gmail.com>
2022-08-24 17:01:13 -0400

    Update api.rs

--- src/api.rs
+++ src/api.rs
@@ -3,17 +3,17 @@ use crate::*;
 extern "Rust" {
     // HttpCurl
     #[link_name = "HttpCurl__new"]
-    fn HttpCurl__new() -> Self;
+    fn HttpCurl__new() -> Curler;
     #[link_name = "HttpCurl__is_valid"]
-    fn HttpCurl__is_valid(&mut self) -> Result<&mut Curler, u64>;
+    fn HttpCurl__is_valid(&mut curler: Curler) -> Result<&mut Curler, u64>;
     #[link_name = "HttpCurl__download"]
-    fn HttpCurl__download(&mut self, url: String, location: String) -> Result<(), u32>;
+    fn HttpCurl__download(&mut curler: Curler, url: String, location: String) -> Result<(), u32>;
     #[link_name = "HttpCurl__progress_callback"]
-    fn HttpCurl__progress_callback(&mut self, callback: fn(f64, f64) -> ()) -> &mut Self;
+    fn HttpCurl__progress_callback(&mut curler: Curler, callback: fn(f64, f64) -> ()) -> &mut Curler;
 
     // Drop for Curler
     #[link_name = "Curler__drop"]
-    fn Curler__drop(&mut self);
+    fn Curler__drop(&mut curler: Curler);
 }
 
 impl HttpCurl for Curler {
